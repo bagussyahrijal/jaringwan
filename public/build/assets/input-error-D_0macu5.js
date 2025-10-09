e2704976dca091647a1304d7aaf199c03c2a8080
Seosootang <abyantaraputra@gmail.com>
2025-10-09 13:25:12 +0700

    build

--- public/build/assets/input-error-D_0macu5.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-BF3J6DCG.js";import{b as o}from"./app-logo-icon-BJLmLHbO.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
