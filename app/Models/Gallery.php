a145abe660ab4eebbdbbabcbc20fb82f5f997ca5
bagussyahrijal <bagussyahrijal123@gmail.com>
2025-10-10 10:41:04 +0700

    fixing bug

--- app/Models/Gallery.php
+++ app/Models/Gallery.php
@@ -14,17 +14,10 @@ class Gallery extends Model
 
     protected $guarded = ['created_at', 'updated_at'];
 
-    protected $appends = ['image_url'];
 
     public function galleryItems()
     {
         return $this->hasMany(GalleryItem::class);
     }
 
-    protected function imageUrl(): Attribute // <-- Buat fungsi accessor ini
-    {
-        return Attribute::make(
-            get: fn () => Storage::disk('public')->url($this->image),
-        );
-    }
 }
