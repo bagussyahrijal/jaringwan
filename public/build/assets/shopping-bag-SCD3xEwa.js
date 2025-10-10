fb31aa0064771598c714b187a2dbbaffd4f336c7
bagussyahrijal <bagussyahrijal123@gmail.com>
2025-10-10 11:19:27 +0700

    remove build

--- public/build/assets/shopping-bag-SCD3xEwa.js
+++
@@ -1,6 +0,0 @@
-import{r,j as t}from"./app-Fi2dGTcM.js";import{c as s}from"./app-logo-icon-DxkZamRy.js";function h({className:n}){const e=r.useId();return t.jsxs("svg",{className:n,fill:"none",children:[t.jsx("defs",{children:t.jsx("pattern",{id:e,x:"0",y:"0",width:"10",height:"10",patternUnits:"userSpaceOnUse",children:t.jsx("path",{d:"M-3 13 15-5M-5 5l18-18M-1 21 17 3"})})}),t.jsx("rect",{stroke:"none",fill:`url(#${e})`,width:"100%",height:"100%"})]})}/**
- * @license lucide-react v0.475.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const a=[["path",{d:"M6 2 3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4Z",key:"hou9p0"}],["path",{d:"M3 6h18",key:"d0wm0j"}],["path",{d:"M16 10a4 4 0 0 1-8 0",key:"1ltviw"}]],p=s("ShoppingBag",a);export{h as P,p as S};
